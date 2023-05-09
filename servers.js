2e8339bb3d5943b0d1b4707bc390a2994a7f5a53
root <root@DESKTOP-31HM7L6.localdomain>
2023-05-09 12:32:47 -0400

    routs for rock paper scissors

--- servers.js
+++ servers.js
@@ -7,3 +7,26 @@ import minimist from 'minimist'
 var arg = minimist(process.argv.slice(2));
 
 const PORT = arg.port || 5000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// check for 200 ok
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+});
+
+// rock paper scissors
+app.get('/app/rps', (req, res) => {
+	res.status(200).send(rps());
+});
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.query.shot));
+});
+app.post('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.body.shot));
+});
+app.get('/app/rps/play/:arg', (req, res) => {
+	res.status(200).send(rps(req.params.arg));
+});
+// rock paper scissors lizard spock
+
