05dc65ad70919f2d92867b9389112cbfa6f28ef5
root <root@DESKTOP-31HM7L6.localdomain>
2023-05-09 12:41:22 -0400

    routes for rock paper scissors lizard spock and fixed file name

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
 #!/usr/bin/env node
+
+import {getRandom, checkValid, checkWin, play, rps, rpsls} from "../lib/rpsls.js"
+import express from 'express'
+const app = express();
+import minimist from 'minimist'
+var arg = minimist(process.argv.slice(2));
+
+const PORT = arg.port || 5000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// check for 200 ok
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+});
+
+// rock paper scissors
+app.get('/app/rps', (req, res) => {
+	res.status(200).send(rps());
+});
+app.get('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.query.shot));
+});
+app.post('/app/rps/play', (req, res) => {
+	res.status(200).send(rps(req.body.shot));
+});
+app.get('/app/rps/play/:arg', (req, res) => {
+	res.status(200).send(rps(req.params.arg));
+});
+// rock paper scissors lizard spock
+app.get('/app/rpsls', (req, res) => {
+	res.status(200).send(rpsls());
+});
+app.get('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.query.shot));
+});
+app.post('/app/rpsls/play', (req, res) => {
+	res.status(200).send(rpsls(req.body.shot));
+});
+app.get('/app/rpsls/play/:arg', (req, res) => {
+	res.status(200).send(rpsls(req.params.arg));
+});
